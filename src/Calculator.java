7fd1c9627b7f56742e0504a335682cd77b64fb51
Claudia Márquez <claudia92@gmail.com>
2016-03-03 18:14:22 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,6 @@
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!. Primer commit. Mas comentarios!");
+		System.out.println("2 + 2 = 4!");
 	}
 }
